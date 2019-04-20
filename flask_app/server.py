46593198ce543f266a64714dde885ae8911d9dc8
yangdanny97 <dzy4@cornell.edu>
2019-04-20 00:59:47 -0400

    updated server to connect to erics vis

--- flask_app/server.py
+++ flask_app/server.py
@@ -5,42 +5,12 @@ from joblib import dump, load
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/classify_multiple', methods = ['POST'])
-def classify_multiple():
-    headline = request.form.get('headline')
-    stances = pd.read_csv("train_stances.csv")
-    bodies = pd.read_csv("train_bodies.csv")
-    def get_body(n):
-        bodies.loc[lambda x: x["Body ID"] == n, "articleBody"].item()
-    relevant_stances = stances[stances["Headline"]==headline]
-    relevant_bodies = [get_body(i[1]) for i in relevant_stances.values()]
-    results = classify_helper(headline, relevant_bodies)
-    return jsonify(results)
-
-@app.route('/classify', methods = ['POST'])
-def classify():
-    headline = request.form.get('headline')
-    body = request.form.get('body')
-    results = classifiy_helper(headline, [body])
-    return jsonify(results)
-
-@app.route('/classify_test', methods = ['POST'])
-def classify_test():
-    headline = ""
-    body = ""
-    results = classifiy_helper(headline, [body])
-    return jsonify(results)
-
 # helper to to most of the heavy lifting- takes in 1 headline and a list of bodies (as strings of text)
 def classify_helper(headline, bodies):
     idf = {}
     with open('idf.json',"r") as idf_file:
         idf = json.load(idf_file)
-    nlp_h = nlp(preprocess(h))
+    nlp_h = nlp(preprocess(headline))
     headline_processed = process_sentence(nlp_h)
     headline_edges = get_edges(nlp_h)
     headline_graph = nx.DiGraph(list(set([(e[0]['token'], e[1]['token']) for e in headline_edges])))
@@ -58,13 +28,13 @@ def classify_helper(headline, bodies):
         body_processed = process_body(nlp_b, idf)
         body_graph = build_graph(nlp_b)
         body_info_rel.append(body_processed)
-        body_info.append(nlp_b, body_graph)
+        body_info.append((nlp_b, body_graph))
 
     features_rel = get_features_rel(headline_info_rel, body_info_rel)
     features_stance, summary_graphs = get_features_stance(headline_info, body_info)
 
-    rel_model = load('saved_models/relevance_detection_trained.joblib')
-    stance_model = load('saved_models/stance_detection_trained.joblib')
+    rel_model = load('../saved_models/relevance_detection_trained.joblib')
+    stance_model = load('../saved_models/stance_detection_trained.joblib')
 
     testing_data = [[],[]]
     for i in range(len(features_rel)):
@@ -87,3 +57,38 @@ def classify_helper(headline, bodies):
         },
         "predictions": predicted
     }
+    return result
+
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template('index.html', data=[])
+
+@app.route('/classify_multiple', methods = ['GET'])
+def classify_multiple():
+    headline = request.form.get('headline')
+    stances = pd.read_csv("train_stances.csv")
+    bodies = pd.read_csv("train_bodies.csv")
+    def get_body(n):
+        bodies.loc[lambda x: x["Body ID"] == n, "articleBody"].item()
+    relevant_stances = stances[stances["Headline"]==headline]
+    relevant_bodies = [get_body(i[1]) for i in relevant_stances.values()]
+    results = classify_helper(headline, relevant_bodies)
+    return render_template('index.html', data=results)
+
+@app.route('/classify', methods = ['GET'])
+def classify():
+    headline = request.form.get('headline')
+    body = request.form.get('body')
+    results = classify_helper(headline, [body])
+    return render_template('index.html', data=results)
+
+@app.route('/classify_test', methods = ['GET'])
+def classify_test():
+    headline = "Julian Assange must face Swedish justice first - MPs and peers"
+    body = """More than 70 MPs and peers have signed a letter urging the home secretary to ensure Julian Assange faces authorities in Sweden if they want his extradition.
+    The Wikileaks founder, who is now in UK custody, was arrested on Thursday after years in Ecuador's London embassy.
+    Sweden is considering whether to reopen an investigation into rape and sexual assault allegations against him.
+    And the US is seeking his extradition in relation to one of the largest ever leaks of government secrets, in 2010.
+    The whistle-blowing website Wikileaks has published thousands of classified documents covering everything from the film industry to national security and war."""
+    results = classify_helper(headline, [body, body])
+    return render_template('index.html', data=results)
--- flask_app/server_helpers.py
+++ flask_app/server_helpers.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 import numpy as np
 import pandas as pd
 import spacy
